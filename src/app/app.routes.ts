16ab8fe46fd2dbdb96f4a11150c3c5339205d170
RomanElements <roman.paslavksy@elements.cloud>
2024-10-17 10:57:09 +0300

    added skeleton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EventsComponent } from './pages/events/events.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { InfoComponent } from './pages/info/info.component';
+import { FundraisingComponent } from './pages/fundraising/fundraising.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'contact-us', component: ContactsComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'fundraising', component: FundraisingComponent },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/pages/fundraising/fundraising.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fundraising',
+  standalone: true,
+  imports: [],
+  templateUrl: './fundraising.component.html',
+  styleUrl: './fundraising.component.scss'
+})
+export class FundraisingComponent {
+
+}
