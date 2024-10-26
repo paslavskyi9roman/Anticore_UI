491aa026200f7ed471f6d701d24a204c89c7e1e4
Roman Paslavskyi <roman.paslavskyi@elements.cloud>
2024-10-26 15:46:16 +0300

    added service to events.component.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyDRopXoJvLts5SXQFbMnkTV5a6SwevA8ok",
+    authDomain: "anticore-lviv.firebaseapp.com",
+    projectId: "anticore-lviv",
+    storageBucket: "anticore-lviv.appspot.com",
+    messagingSenderId: "147612515351",
+    appId: "1:147612515351:web:e4c9f7c15ef63b8aacb052",
+    measurementId: "G-KBRNT9VERQ"
   }
 };
